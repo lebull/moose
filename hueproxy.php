f655e8e5468c767437a84fd6aafc1a9f12c6ab22
lebull <bulldogboy912000@yahoo.com>
2017-01-06 12:48:46 -0600

    Added getallheaders() for PHP version <5.4

--- hueproxy.php
+++ hueproxy.php
@@ -2,6 +2,23 @@
 
     include 'hueproxysettings.php';
 
+    if (!function_exists('getallheaders')) 
+    { 
+        function getallheaders() 
+        { 
+            $headers = ''; 
+           foreach ($_SERVER as $name => $value) 
+           { 
+               if (substr($name, 0, 5) == 'HTTP_') 
+               { 
+                   $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value; 
+               } 
+           } 
+           return $headers; 
+        } 
+    } 
+
+
     error_reporting(E_ALL);
     ini_set('display_errors', 1);
 
