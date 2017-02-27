bfbfa8383be6f44c913755565996b48f4ad6c181
lebull <bulldogboy912000@yahoo.com>
2017-02-27 12:58:36 +0000

    Adjusted proxyAddress

--- js/huesettings.js
+++ js/huesettings.js
@@ -1,7 +1,4 @@
 /*global huesettings*/
 huesettings = { 
-    user: "4MdCraSegQTzgr1MJ8BkfpgKru2eSJC5EYTyvYST",
-    //address: "192.168.1.100",
-    address: "http://73.2.215.174",
-    proxyPath: "/hueproxy.php?loc=/api/"
+    proxyAddress: "hueproxy.php",
 }
